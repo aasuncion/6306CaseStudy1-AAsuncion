8390f526e80e2b711a094962bc2a19a6941a9df7
aasuncion <aasuncion@mail.smu.edu>
2017-03-08 20:02:04 -0800

    Add files via upload

---
+++ AAsuncionCase1.R
@@ -0,0 +1,3 @@
+setwd("/Users/albertasuncion_mac/Documents/SMU/6306 - Intro to DS/rWD/")
+fedstats <- read.csv(file="getdata%2Fdata%2FEDSTATS_Country.csv",header=TRUE)
+gdp <- read.csv(file="getdata%2Fdata%2FGDP.csv",header=TRUE,skip=3)
